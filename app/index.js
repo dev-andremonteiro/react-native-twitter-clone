cc4d4f342075c2fbccce49536c2fa58b389545e5
Andre Monteiro <andrevmonteiro.eng@gmail.com>
2021-02-13 16:06:31 -0400

    Upgrade to SDK 40

--- app/index.js
+++ app/index.js
@@ -1,13 +1,15 @@
-import React from "react";
-import Navigator from "./Navigate";
-import { Asset } from "expo";
-import { Animated, Easing } from "react-native";
+import React from 'react';
+import { Animated, Easing } from 'react-native';
 
-const TwitterLogo = require("../assets/icon.png");
+import { Asset } from 'expo-asset';
+
+import Navigator from './Navigate';
+
+const TwitterLogo = require('../assets/icon.png');
 
 function cacheImages(images) {
-  return images.map(image => {
-    if (typeof image === "string") {
+  return images.map((image) => {
+    if (typeof image === 'string') {
       return Image.prefetch(image);
     } else {
       return Asset.fromModule(image).downloadAsync();
@@ -19,7 +21,7 @@ class Loading extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      iconSize: new Animated.Value(80)
+      iconSize: new Animated.Value(80),
     };
   }
 
@@ -27,7 +29,7 @@ class Loading extends React.Component {
     Animated.timing(this.state.iconSize, {
       toValue: 2500,
       duration: 350,
-      easing: Easing.back(0.4)
+      easing: Easing.back(0.4),
     }).start(() => this.props.animationEnd());
 
   render() {
@@ -35,17 +37,17 @@ class Loading extends React.Component {
       <Animated.View
         style={{
           flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-          backgroundColor: "#1DA1F2"
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: '#1DA1F2',
         }}
       >
         <Animated.Image
-          name={"logo-twitter"}
+          name={'logo-twitter'}
           style={{
-            alignSelf: "center",
+            alignSelf: 'center',
             width: this.state.iconSize,
-            height: this.state.iconSize
+            height: this.state.iconSize,
           }}
           source={TwitterLogo}
         />
@@ -58,25 +60,25 @@ export default class RootComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoading: true
+      isLoading: true,
     };
   }
 
   async _loadAssets() {
     const imageAssets = cacheImages([
-      require("../assets/topMore.png"),
-      require("../assets/thunder.png"),
-      require("../assets/topStar.png"),
-      require("../assets/topGear.png"),
-      require("../assets/tweet.png"),
-      require("../assets/message.png"),
-      require("../assets/wizardsunite.png"),
-      require("../assets/avatar/user1.jpg"),
-      require("../assets/avatar/user2.jpg"),
-      require("../assets/avatar/user3.jpg"),
-      require("../assets/avatar/user4.jpg"),
-      require("../assets/avatar/user5.jpg"),
-      require("../assets/avatar/user6.jpg")
+      require('../assets/topMore.png'),
+      require('../assets/thunder.png'),
+      require('../assets/topStar.png'),
+      require('../assets/topGear.png'),
+      require('../assets/tweet.png'),
+      require('../assets/message.png'),
+      require('../assets/wizardsunite.png'),
+      require('../assets/avatar/user1.jpg'),
+      require('../assets/avatar/user2.jpg'),
+      require('../assets/avatar/user3.jpg'),
+      require('../assets/avatar/user4.jpg'),
+      require('../assets/avatar/user5.jpg'),
+      require('../assets/avatar/user6.jpg'),
     ]);
     await Promise.all([...imageAssets]).then(this.load._startAnimation);
   }
@@ -87,7 +89,7 @@ export default class RootComponent extends React.Component {
     if (this.state.isLoading)
       return (
         <Loading
-          ref={ref => (this.load = ref)}
+          ref={(ref) => (this.load = ref)}
           animationEnd={() => this.setState({ isLoading: false })}
         />
       );
